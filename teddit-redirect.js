3e6b7d3518abb523e16d933dde9b7547c0d662d1
Simon Cedergren <simon@tuxflux.se>
2020-12-07 13:57:50 +0100

    Update teddit-redirect.js

--- teddit-redirect.js
+++ teddit-redirect.js
@@ -1,4 +1,4 @@
 const original_url = window.location.href
-const new_url = original_url.replace('old.reddit.com','teddit.net').replace('reddit.com','teddit.com')
+const new_url = original_url.replace('old.reddit.com','teddit.net').replace('reddit.com','teddit.net')
 window.location.replace(new_url)
 
