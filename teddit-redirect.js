8f1d455ef188a564c89371ba775d51e84f38e89c
Thomas Lange <code@nerdmind.de>
2021-04-20 16:03:46 +0200

    Rewrite main script (use webRequest approach)
    This commit rewrites the main script teddit-redirect.js which is
    registered as background script within the manifest.json and uses the
    webRequest approach. This makes it possible to rewrite the request to
    the choosen teddit instance before it reaches Reddit's servers.

--- teddit-redirect.js
+++ teddit-redirect.js
@@ -1,17 +1,51 @@
-function onError(error) {
-  console.log(`Error: ${error}`);
+/*
+Teddit Redirect
+*/
+async function tedditRedirectHandler(details) {
+
+	// Default URL of teddit instance
+	let tedditUrl = "https://teddit.net";
+
+	// Target URL of the captured webRequest event
+	const initialTargetUrl = details.url;
+
+	// Try to get the teddit instance URL from sync storage
+	const result = await browser.storage.sync.get("instance");
+
+	if(result.instance !== undefined) {
+		tedditUrl = "https://" + result.instance;
+	}
+
+	const rewrittenTargetUrl = initialTargetUrl.replace(
+		/^http(?:s)?:\/\/(?:(?:www|old).)?reddit.com(?:\/)?(.*)/i,
+		tedditUrl.replace(/(\/+)$/, "") + "/$1"
+	);
+
+	/*
+	Return a webRequest.BlockingResponse object.
+	https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/webRequest/BlockingResponse
+	*/
+	blockingResponse = {};
+	blockingResponse.redirectUrl = rewrittenTargetUrl;
+
+	return blockingResponse;
 }
 
-function onSettingsRead(item) {
-  let instance = "teddit.net";
-  if (item.instance) {
-    instance = item.instance;
-  }
-
-  const original_url = window.location.href
-  const new_url = original_url.replace('old.reddit.com', instance).replace(/(www\.)?reddit.com/, instance)
-  window.location.replace(new_url)
+/*
+Target URL filter for the webRequest.onBeforeRequest event listener.
+https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/webRequest/RequestFilter
+*/
+webRequestFilter = {
+	urls: [
+		"*://reddit.com/*",
+		"*://www.reddit.com/*",
+		'*://old.reddit.com/*'
+	]
 }
 
-let getting = browser.storage.sync.get("instance");
-getting.then(onSettingsRead, onError);
+/*
+Add a webRequest.onBeforeRequest event listener.
+https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/webRequest/onBeforeRequest
+*/
+browser.webRequest.onBeforeRequest.addListener(
+	tedditRedirectHandler, webRequestFilter, ["blocking"]);
